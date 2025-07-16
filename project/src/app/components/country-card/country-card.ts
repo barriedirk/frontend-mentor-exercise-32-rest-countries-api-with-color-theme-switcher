de9466e6bbeb63984d62c5cc21b1c6c6889acd0e
Barrie Freyre <barriedirk@yahoo.com>
2025-07-15 22:51:26 -0500

    add styles grid

---
+++ project/src/app/components/country-card/country-card.ts
@@ -0,0 +1,22 @@
+import { DecimalPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input, Output } from '@angular/core';
+
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-country-card',
+  standalone: true,
+  imports: [DecimalPipe, RouterLink],
+  templateUrl: './country-card.html',
+  styleUrl: './country-card.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CountryCard {
+  @Input() cca3!: string;
+  @Input() href!: string;
+  @Input() imageFlag!: string;
+  @Input() name!: string;
+  @Input() population!: number;
+  @Input() region!: string;
+  @Input() capital!: string;
+}
--- project/src/app/components/result-grid/result-grid.ts
+++ project/src/app/components/result-grid/result-grid.ts
@@ -1,11 +1,68 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
+
+import { CountryCard } from '@components/country-card/country-card';
+
+import { RestCountries } from '@services/restcountries';
+
+import { Country } from '@interfaces/country';
+
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-result-grid',
   standalone: true,
-  imports: [],
+  imports: [CountryCard],
   templateUrl: './result-grid.html',
   styleUrl: './result-grid.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ResultGrid {}
+export class ResultGrid implements OnInit {
+  private countriesService = inject(RestCountries);
+
+  countries = signal<Country[]>([]);
+
+  mockup = [
+    {
+      cca3: '',
+      href: '',
+      alt: '',
+      imageFlag: '',
+      name: 'Germany',
+      population: 34343,
+      region: 'Europe',
+      capital: 'Berlin',
+    },
+    {
+      cca3: '',
+      href: '',
+      alt: '',
+      imageFlag: '',
+      name: 'Germany',
+      population: 34343,
+      region: 'Europe',
+      capital: 'Berlin',
+    },
+    {
+      cca3: '',
+      href: '',
+      alt: '',
+      imageFlag: '',
+      name: 'Germany',
+      population: 34343,
+      region: 'Europe',
+      capital: 'Berlin',
+    },
+  ];
+
+  ngOnInit() {
+    this.countriesService
+      .fetchCountries()
+      .pipe(
+        tap((data) => {
+          console.log(data);
+          this.countries.set(data);
+        }),
+      )
+      .subscribe();
+  }
+}
